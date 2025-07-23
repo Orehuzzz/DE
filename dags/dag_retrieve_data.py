ced3ec54435f25e5b37d402aecc4875af116443c
Orehuzzz <oreshin.vitalik@gmail.com>
2025-07-23 23:15:09 +0300

    update dag_retrieve_data.py

--- dags/dag_retrieve_data.py
+++ dags/dag_retrieve_data.py
@@ -8,16 +8,17 @@ import requests
 
 default_args = {
     'owner':'etl_user',
-    'depends_on_past':True,
+    # 'depends_on_past':True,
     'start_date':datetime(2025, 7, 17),
-    'retries':1
+    'retries':0
 }
 
-#Можно через airflow.decorators import task ?
 def take_csv(**context):
     csv_file = pd.read_csv('./dags/comments.csv', sep=';')
     context['ti'].xcom_push(key='csv_file', value=csv_file)
     return(csv_file)
+
+
 
 def take_xlsx(**context):
     xlsx_file = pd.read_excel('./dags/users.xlsx')
@@ -30,8 +31,8 @@ def take_api(**context):
     context['ti'].xcom_push(key='api', value=api_data)
     return(df)
 
-with DAG('retrieve_data', default_args=default_args, schedule_interval='@daily', catchup=True, 
-          max_active_tasks=3, max_active_runs=1, tags=['dag_retrieves_data', 'first_ex']) as dag:
+with DAG('retrieve_data', default_args=default_args, schedule_interval='@daily', catchup=False, 
+           tags=['dag_retrieves_data', 'first_ex']) as dag:
     
     with TaskGroup('data_preparation') as prep_group:
 
